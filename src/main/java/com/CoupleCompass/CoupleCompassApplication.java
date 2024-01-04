677699263fbf64152abb8022ea0c33452d500c49
jeongseunghyun <seunghyun.jeong@weaverloft.com>
2024-01-04 13:58:18 +0900

    :zap: 프로젝트 시작

---
+++ src/main/java/com/CoupleCompass/CoupleCompassApplication.java
@@ -0,0 +1,13 @@
+package com.CoupleCompass;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoupleCompassApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoupleCompassApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/CoupleCompass/CoupleCompassApplicationTests.java
@@ -0,0 +1,13 @@
+package com.CoupleCompass;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoupleCompassApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
